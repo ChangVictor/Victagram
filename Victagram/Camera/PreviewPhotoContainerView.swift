5f7663adeb17b750fa6fe6994a986fa5cfea685f
Victor Chang <v.chang.yu@gmail.com>
2018-08-30 17:03:46 -0300

    handleSave photo

--- Victagram/Camera/PreviewPhotoContainerView.swift
+++ Victagram/Camera/PreviewPhotoContainerView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Photos
 
 class PreviewPhotoContainerView: UIView {
 	
@@ -22,6 +23,65 @@ class PreviewPhotoContainerView: UIView {
 		return button
 	}()
 	
+	let saveButton: UIButton = {
+		let button = UIButton(type: .system)
+		button.setImage(#imageLiteral(resourceName: "save_shadow").withRenderingMode(.alwaysOriginal), for: .normal)
+		button.addTarget(self, action: #selector(handleSave), for: .touchUpInside)
+		return button
+	}()
+	
+	@objc fileprivate func handleSave() {
+		
+		print("Handling Save...")
+		guard let previewImage = previewImageView.image else { return }
+		let library = PHPhotoLibrary.shared()
+		
+		library.performChanges({
+			
+			PHAssetChangeRequest.creationRequestForAsset(from: previewImage)
+			
+		}) { (succes, error) in
+			if let error = error {
+				print("Failed to save image to Photo Library: ", error)
+				return
+			}
+			
+			print("Succesfully saved image to Library")
+			DispatchQueue.main.async {
+				let savedLabel = UILabel()
+				savedLabel.numberOfLines = 0
+				savedLabel.font = UIFont.boldSystemFont(ofSize: 18)
+				savedLabel.text =  "Saved Succesfully"
+				savedLabel.textColor = .white
+				savedLabel.textAlignment = .center
+				savedLabel.backgroundColor = UIColor(white: 0, alpha: 0.3)
+				savedLabel.frame = CGRect(x: 0, y: 0, width: 150, height: 80)
+				savedLabel.center = self.center
+				
+				self.addSubview(savedLabel)
+				
+				savedLabel.layer.transform = CATransform3DMakeScale(0, 0, 0)
+				
+				UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+					savedLabel.layer.transform = CATransform3DMakeScale(1, 1, 1)
+				}, completion: { (completed) in
+					//completed
+					UIView.animate(withDuration: 0.5, delay: 0.75, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+						
+						savedLabel.layer.transform = CATransform3DMakeScale(0.1, 0.1, 0.1)
+						
+					}, completion: { (_) in
+						
+						savedLabel.removeFromSuperview()
+						
+					})
+					
+				})
+			}
+			
+		}
+	}
+	
 	@objc fileprivate func handleCancel() {
 		self.removeFromSuperview()
 	}
@@ -36,6 +96,9 @@ class PreviewPhotoContainerView: UIView {
 		
 		addSubview(cancelButton)
 		cancelButton.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 12, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 50, height: 50)
+		
+		addSubview(saveButton)
+		saveButton.anchor(top: nil, left: leftAnchor, bottom: bottomAnchor, right: nil, paddingTop: 0, paddingLeft: 24, paddingBottom: 24, paddingRight: 24, width: 50, height: 50)
 	}
 	
 	required init?(coder aDecoder: NSCoder) {
